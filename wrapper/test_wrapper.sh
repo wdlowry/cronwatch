883823a4102ed27963f18f5e8bf6cc42075209da
wdlowry <wdlowry@7b302cb2-a02d-11dd-a887-7ffe1a420f8d>
2009-01-06 21:53:10 +0000

    Added config file and queue dir options

--- wrapper/test_wrapper.sh
+++ wrapper/test_wrapper.sh
@@ -10,12 +10,40 @@
 
 # Should fail with an error without any command line options
 test_fail_noargs () {
-    O=`./wrapper.sh 2>&1 | head -1`
-    ./wrapper.sh > /dev/null 2>&1
+    O=`./wrapper 2>&1 | head -1`
+    ./wrapper > /dev/null 2>&1
     R="$?"
 
     assertEquals 'ERROR: missing argument' "$O"
     assertEquals "1" "$R"
+}
+
+# -c should set the configuration file
+test_opt_config () {
+    O=`./wrapper -z CONFIGFILE /dev/null`
+    assertEquals '' "$O"
+
+    O=`./wrapper -c configfile -z CONFIGFILE /dev/null`
+    assertEquals 'configfile' "$O"
+}
+
+# -h should display the usage and quit
+test_opt_help () {
+    O=`./wrapper -h 2>&1 | head -1`
+    ./wrapper -h > /dev/null 2>&1
+    R="$?"
+    
+    assertEquals 'usage: ./wrapper [options] executable [arguments]' "$O"
+    assertEquals "0" "$R"
+}
+
+# -q should set the queue directory
+test_opt_queue () {
+    O=`./wrapper -z QUEUEDIR /dev/null`
+    assertEquals '/var/lib/cronwatch/wrapper' "$O"
+
+    O=`./wrapper -q queuedir -z QUEUEDIR /dev/null`
+    assertEquals 'queuedir' "$O"
 }
 
 . ../tools/shunit2
