643de1b7ea08c7180c7ebc8d90dad005c16a3ffe
wdlowry <wdlowry@7b302cb2-a02d-11dd-a887-7ffe1a420f8d>
2009-01-06 15:53:55 +0000

    Added default header

--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # $Id$
-# cronwatch wrapper - Capture output from cron jobs and store it for processing
-# 
-
+# vim:ft=sh:sw=4:sta:et
+#
+# wrapper - Capture output from cron jobs and store it for processing
+#
+# Written by David Lowry
+# Released under the GPL version 2
+#
 
