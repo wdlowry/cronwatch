95d091db127180e5af3833681361fae7d2084826
wdlowry <wdlowry@7b302cb2-a02d-11dd-a887-7ffe1a420f8d>
2009-01-06 18:34:06 +0000

    Added option/argument handling

--- wrapper/test_wrapper.sh
+++ wrapper/test_wrapper.sh
@@ -8,4 +8,14 @@
 # Released under the GPL version 2
 #
 
+# Should fail with an error without any command line options
+test_fail_noargs () {
+    O=`./wrapper.sh 2>&1 | head -1`
+    ./wrapper.sh > /dev/null 2>&1
+    R="$?"
+
+    assertEquals 'ERROR: missing argument' "$O"
+    assertEquals "1" "$R"
+}
+
 . ../tools/shunit2
--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -8,3 +8,37 @@
 # Released under the GPL version 2
 #
 
+# Print out the usage
+usage () {
+    cat << EOF
+usage: $0 [options] executable [arguments]
+
+options:
+  -c                    config file
+  -h                    this help message
+  -q                    queue directory
+  -u                    unique job name
+EOF
+}
+
+# Check for necessary options
+if [ -z "$1" ] ; then
+    echo >&2 'ERROR: missing argument'
+    usage >&2
+    exit 1
+fi
+
+# Handle the command line options
+while getopts c:hq:u: OPT; do
+    case "$OPT" in
+        c) ;;
+        h) ;;
+        q) ;;
+        u) ;;
+        ?) ;;
+    esac
+done
+
+shift $(($OPIND - 1))
+
+
