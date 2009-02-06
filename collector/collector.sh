ee5b163f5815a1adfcb7861396dc2d38df93b944
wdlowry <wdlowry@7b302cb2-a02d-11dd-a887-7ffe1a420f8d>
2009-02-06 18:42:46 +0000

    Added collector skeleton stuff

---
+++ collector/collector.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# $Id$
+# vim:ft=sh:sw=4:sta:et
+#
+# collector - Move cron jobs status and output to central server
+# Copyright (C) 2009 David Lowry < wdlowry _remove_ at _remove_ gmail dot com >
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+
+# Print out the usage
+usage () {
+    cat << EOF
+usage: $0 [options] executable [arguments]
+
+EOF
+}
+
+# Print an error message and exit
+error () {
+    echo >&2 "ERROR: $1" 
+    exit 1
+}
+
