c46a55e3a8ddd6e1629777af374bae8ac67ca729
wdlowry <wdlowry@7b302cb2-a02d-11dd-a887-7ffe1a420f8d>
2010-08-13 17:57:52 +0000

    Tweaked paver file

--- trunk/pavement.py
+++ trunk/pavement.py
@@ -1,19 +1,34 @@
 from paver.easy import *
-from paver.setuputils import setup
+from paver.setuputils import setup, install_distutils_tasks
+    
+setup(
+    name = 'cronwatch',
+    version = '1.0',
+    description = 'A script that monitors cron job output',
+    author = 'David Lowry',
+    author_email = 'wdlowry@gmail.com',
+    url = 'http://code.google.com/p/cronwatch/',
+    license = 'GPL2',
+    scripts = ['cronwatch'],
+    test_suite = 'nose.collector',
+    install_requires=['configobj']
+)
 
-setup(name = 'cronwatch',
-      version = '1.0',
-      url = 'http://code.google.com/p/cronwatch/',
-      author = 'David Lowry',
-      author_email = 'wdlowry@gmail.com',
-      scripts = ['cronwatch'],
-      description = 'A script that monitors cron job output',
-      license = 'GPL2',
-      test_suite = 'test_cronwatch')
-
+options(
+    sphinx=Bunch(
+        docroot = 'docs',
+        builddir = 'build',
+        sourcedir = 'source'
+    )
+)
 
 @task
-@needs('generate_setup', 'minilib', 'setuptools.command.sdist')
+@needs('paver.doctools.html')
+def html():
+    pass
+
+@task
+@needs('generate_setup', 'minilib', 'html', 'setuptools.command.sdist')
 def sdist():
     '''Overrides sdist to make sure that our setup.py is generated.'''
 
